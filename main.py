8490efd795c682312ba2a23d96409725811d8693
Ram Devakumar <ramdevakumar20@gmail.com>
2025-04-07 23:23:52 -0400

    Added error checking for self bot message

--- main.py
+++ main.py
@@ -6,15 +6,25 @@ from dotenv import load_dotenv
 load_dotenv()  # load variables from .env into environment
 
 SECRET_TOKEN = os.getenv("SECRET_TOKEN")
+ACTIVE = os.getenv("ACTIVE", "False") == "True"
 
 class Client(discord.Client):
     async def on_ready(self):
         print("Running Gym Bot")
 
     async def on_message(self, message):
-        userMessage = message.content.lower()[1:] #to ignore !
-        if userMessage == "war" or userMessage == "mccomas":
-            await message.channel.send(scraper.scrapeGym(userMessage))
+        if message.content[0] == "!":
+            if message.author == self.user:
+                return
+
+            if not ACTIVE:
+                await message.channel.send("Bot Hours: 9am to 11pm")
+                return
+
+            userMessage = message.content.lower()[1:] #to ignore !
+            if userMessage == "war" or userMessage == "mccomas":
+                await message.channel.send(scraper.scrapeGym(userMessage))
+
 
 
 intents = discord.Intents.default()
